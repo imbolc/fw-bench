a4d6387690fdc53407564680b425b0e4d0076f8a
imbolc <imbolc@imbolc.name>
2013-09-13 14:08:17 +0700

    update django app

--- django_app/settings.py
+++ django_app/settings.py
@@ -1,5 +1,4 @@
 DEBUG = False
-DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ROOT_URLCONF = 'urls'
@@ -17,3 +16,28 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
 )
+
+ALLOWED_HOSTS = '*'
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+            },
+        },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+            'propagate': True,
+            },
+        }
+    }
--- django_app/views.py
+++ django_app/views.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
 
 
 ROWS = [[{'row': row, 'col': col} for col in xrange(10)] for row in xrange(10)]
